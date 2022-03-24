989e5f64f16d49c4f55379ebe0e69d26817612e9
Alex Riina <alex.riina@gmail.com>
2022-03-24 19:41:50 -0400

    fuck the local

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
-import pandoc.types
 import argparse
-from functools import cache
 import configparser
+import pathlib
+import re
+from functools import lru_cache
 
+import pandoc
 from pyzotero.zotero import Zotero
 
 
-def zotero_local_info(doi, database) -> str:
-    engine = create_engine(database, echo=False)
-    Session = sessionmaker(bind=engine)
-
-    with Session() as session:
-        session.query(Item)
-
-
-@cache
-def zotero_items():
-    zot = zotero.Zotero(
-        config["zotero"]["library"],
-        config["zotero"]["library_type"],
-        config["zotero"]["api_key"],
-    )
-
-    return zot.items()
+@lru_cache()
+def zotero_items(library, library_type, api_key):
+    zot = Zotero(library, library_type, api_key)
+    items = zot.items()
+    return items
 
 
-def filename(title):
+def make_filename(title):
     """ convert pubication title to safe filename for obsidian """
     title = title.replace(":", " - ")
-    return title
+    return title + ".md"
 
 
-def write_file(title, file):
-    file.write("---")
-    file.write("type:article")
-    file.write(f"title:{title}")
-    # file.write(f"zotero:zotero://open-pdf/library/items/{zotero_id}")
-    file.write("---")
+def write_file(title, contents, file):
+    file.write("---\n")
+    file.write("type:article\n")
+    file.write(f"title:{title}\n")
+    file.write("---\n")
+    file.write("\n")
+    file.write(contents)
 
 
 def find_doi(items, doi):
     for item in items:
         try:
-            if item['data']['DOI'] == doi:
+            if item["data"]["DOI"] == doi:
                 return item
         except KeyError:
             pass
@@ -52,16 +43,18 @@ def find_doi(items, doi):
 def find_attachment(items, parent_item):
     for item in items:
         try:
-            if item['data']['parentItem'] == item['key']:
+            if item["data"]["parentItem"] == parent_item["key"]:
                 return item
         except KeyError:
             pass
-    raise ValueError("no matching DOI found")
+    raise ValueError("no matching attachment found")
 
 
 def html_to_markdown(note):
     parsed = pandoc.read(note, format="html")
-    return pandoc.write(parsed, format="markdown")
+    output = pandoc.write(parsed, format="markdown", options=["--wrap=none"])
+    output = re.sub(r'\\"(.*)\\"', r"> \1", output)
+    return output
 
 
 def main():
@@ -74,17 +67,30 @@ def main():
         "--vault",
         help="obsidian vault, defaults to config",
         default=config["obsidian"]["vault"],
+        type=pathlib.Path,
     )
 
     args = parser.parse_args()
 
-    items = zotero_items()
+    items = zotero_items(
+        config["zotero"]["library"],
+        config["zotero"]["library_type"],
+        config["zotero"]["api_key"],
+    )
     item = find_doi(items, args.doi)
-    attachment = find_attachment(items, item)
-    markdown = html_to_markdown(attachment)
+    filename = make_filename(item["data"]["title"])
 
+    attachment = find_attachment(items, item)
+    markdown = html_to_markdown(attachment["data"]["note"])
+
+    print(f"writing {args.vault / filename}")
     with open(args.vault / filename, "x") as fp:
         write_file(
-            title=item["title"],
+            title=item["data"]["title"],
+            contents=markdown,
             file=fp,
         )
+
+
+if __name__ == "__main__":
+    main()
